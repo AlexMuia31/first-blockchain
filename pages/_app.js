0037dcdacc3d0513b881df4e726c32b75655f3bb
AlexMuia31 <alexmuia31@gmail.com>
2022-02-02 11:19:50 +0300

    wallect connect and dashboard

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
+
+
+const supportedChainIds = [4]
+const connectors = {
+  injected: {}
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    //wrapping the App with the ThirdwebWeb3Provider authentication
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
+
 }
 
 export default MyApp
